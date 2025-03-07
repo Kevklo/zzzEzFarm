b95d0bea65d1f79f710f8777c3055949219a8b9a
Kunklo <126540566+Kunklo@users.noreply.github.com>
2025-03-07 16:18:21 -0300

    style: better characterCard

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,18 +2,23 @@ import { Route, Routes } from "react-router-dom"
 import { FarmingPage } from "../farm/pages/FarmingPage"
 import { CharacterGrid } from './../farm/pages/CharacterGrid';
 import { CharacterAdder } from "../farm/pages/CharacterAdder";
+import { NavBar } from "../UI/NavBar";
+
 
 export const AppRouter = () => {
 
   const chars = [{name: 'Burnice', attribute: 'Fire', type: 'Anomaly', smallImg: `/assets/burnice_small.jpg`, bigImg: `assets/burnice_big.webp`}] // Temporary untill API is implemented
 
   return (
-    <Routes>
+    <>
+      <NavBar />
+      <Routes>
 
-      <Route path="/*" element={ <FarmingPage /> }/>
-      <Route path="/charactergrid" element={ <CharacterGrid /> }></Route>
-      <Route path="/characteradder" element={ <CharacterAdder chars={ chars }/> }></Route>
+        <Route path="/*" element={ <FarmingPage /> }/>
+        <Route path="/charactergrid" element={ <CharacterGrid chars={ chars }/> }></Route>
+        <Route path="/characteradder" element={ <CharacterAdder chars={ chars }/> }></Route>
       
-    </Routes>
+      </Routes>
+    </>
   )
 }
