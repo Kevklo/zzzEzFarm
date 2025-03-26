0dc1d0de5d9dfe057ab569ebd869ef152923e044
Kunklo <126540566+Kunklo@users.noreply.github.com>
2025-03-25 23:25:21 -0300

    coreSkillTreshold adjusted

--- src/mock/coreSkillTreshold.js
+++ src/mock/coreSkillTreshold.js
@@ -1,4 +1,4 @@
-export const coreSkillLevelTreshold = [{maxCoreLevel: 1, levelNeeded: 1}, {maxCoreLevel: 3, levelNeeded: 15},
-  {maxCoreLevel:5 , levelNeeded: 25}, {maxCoreLevel: 7, levelNeeded: 35}, {maxCoreLevel: 9, levelNeeded: 45},
-  {maxCoreLevel: 11, levelNeeded: 55}, {maxCoreLevel: 12, levelNeeded: 60}
+export const coreSkillLevelTreshold = [{maxCoreLevel: 0, levelNeeded: 1}, {maxCoreLevel: 1, levelNeeded: 15},
+  {maxCoreLevel:2 , levelNeeded: 25}, {maxCoreLevel: 3, levelNeeded: 35}, {maxCoreLevel: 4, levelNeeded: 45},
+  {maxCoreLevel: 5, levelNeeded: 55}, {maxCoreLevel: 6, levelNeeded: 60}
 ]
